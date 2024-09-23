9f7e85e583bc7e3b47b5bfda5498097b9e4b773d
Harish-C-15 <harish1ga18cs060@gmail.com>
2024-09-23 09:44:02 +0530

    Update and rename main.cpp to Color_Pair_main.cpp

---
+++ Color_Pair_main.cpp
@@ -0,0 +1,19 @@
+#include <Color_Pair.hpp>
+#include <iostream>
+
+ColorPair GetColorFromPairNumber(int pairNumber) 
+{
+        int zeroBasedPairNumber = pairNumber - 1;
+        MajorColor majorColor = 
+            (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+        MinorColor minorColor =
+            (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+        return ColorPair(majorColor, minorColor);
+}
+
+    int GetPairNumberFromColor(MajorColor major, MinorColor minor) 
+{
+        return major * numberOfMinorColors + minor + 1;
+}
+
+
