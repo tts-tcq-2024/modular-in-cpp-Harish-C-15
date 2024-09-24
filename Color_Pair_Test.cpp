d098febd1500a7314b5dc56b139439d2dc1772b2
Harish-C-15 <harish1ga18cs060@gmail.com>
2024-09-24 09:04:44 +0530

    Update Color_Pair_Test.cpp

--- Color_Pair_Test.cpp
+++ Color_Pair_Test.cpp
@@ -5,8 +5,7 @@ void testNumberToPair(int pairNumber,
     TelCoColorCoder::MajorColor expectedMajor,
     TelCoColorCoder::MinorColor expectedMinor)
 {
-    TelCoColorCoder::ColorPair colorPair =
-        TelCoColorCoder::GetColorFromPairNumber(pairNumber);
+    TelCoColorCoder::ColorPair colorPair = GetColorFromPairNumber(pairNumber);
     std::cout << "Got pair " << colorPair.ToString() << std::endl;
     assert(colorPair.getMajor() == expectedMajor);
     assert(colorPair.getMinor() == expectedMinor);
@@ -17,7 +16,7 @@ void testPairToNumber(
     TelCoColorCoder::MinorColor minor,
     int expectedPairNumber)
 {
-    int pairNumber = TelCoColorCoder::GetPairNumberFromColor(major, minor);
+    int pairNumber = GetPairNumberFromColor(major, minor);
     std::cout << "Got pair number " << pairNumber << std::endl;
     assert(pairNumber == expectedPairNumber);
 }
