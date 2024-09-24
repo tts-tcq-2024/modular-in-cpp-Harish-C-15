710ca4d86f6c2c80db16307c69e2a1b85aa08940
Harish-C-15 <harish1ga18cs060@gmail.com>
2024-09-24 09:55:17 +0530

    Update ColorPairData.hpp

--- ColorPairData.hpp
+++ ColorPairData.hpp
@@ -7,8 +7,14 @@
     enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
     enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
-    extern const char* MajorColorNames[];
-    extern const int numberOfMajorColors;
-    extern const char* MinorColorNames[];
-    extern const int numberOfMinorColors;
+    extern const char* MajorColorNames[] = {
+        "White", "Red", "Black", "Yellow", "Violet"
+    };
+    extern const int numberOfMajorColors =
+        sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+    extern const char* MinorColorNames[] = {
+        "Blue", "Orange", "Green", "Brown", "Slate"
+    };
+    extern const int numberOfMinorColors =
+        sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 #endif 
