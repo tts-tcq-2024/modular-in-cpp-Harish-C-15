43821f06477af1cb78557a1cc3294bad25bc79e0
Harish-C-15 <harish1ga18cs060@gmail.com>
2024-09-24 09:47:30 +0530

    Update ColorPairManual.cpp

--- ColorPairManual.cpp
+++ ColorPairManual.cpp
@@ -25,6 +25,17 @@ void printColorCodingManual() {
 }
 int main()
 {
+    const char* MajorColorNames[] = {
+        "White", "Red", "Black", "Yellow", "Violet"
+    };
+    const int numberOfMajorColors =
+        sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+    const char* MinorColorNames[] = {
+        "Blue", "Orange", "Green", "Brown", "Slate"
+    };
+    const int numberOfMinorColors =
+        sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+    
     printColorCodingManual();
     return 0;
 }
