b6d3e1f14f2af2ca7c990f3bbadc043650abae0d
Harish-C-15 <harish1ga18cs060@gmail.com>
2024-09-25 11:55:38 +0530

    Update and rename ColorPairData.hpp to ColorPairData.cpp

---
+++ ColorPairData.cpp
@@ -0,0 +1,26 @@
+#include "ColorPair.hpp"
+
+namespace Color
+{
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+const char* MajorColorNames[] = { "White", "Red", "Black", "Yellow", "Violet" };
+const int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+const char* MinorColorNames[] = { "Blue", "Orange", "Green", "Brown", "Slate"};
+const int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
+ColorPair GetColorFromPairNumber(int pairNumber) 
+{
+        int zeroBasedPairNumber = pairNumber - 1;
+        MajorColor majorColor = 
+            (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+        MinorColor minorColor =
+            (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+        return ColorPair(majorColor, minorColor);
+}
+    int GetPairNumberFromColor(MajorColor major, MinorColor minor) 
+{
+        return major * numberOfMinorColors + minor + 1;
+}
+
+}
