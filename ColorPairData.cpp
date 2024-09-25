ab509faa525925c3c1c7f2bcee63dbde259bb5bf
Harish-C-15 <harish1ga18cs060@gmail.com>
2024-09-25 13:25:38 +0530

    Update ColorPairData.cpp

--- ColorPairData.cpp
+++ ColorPairData.cpp
@@ -2,8 +2,6 @@
 
 namespace Color
 {
-enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 const char* MajorColorNames[] = { "White", "Red", "Black", "Yellow", "Violet" };
 const int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 const char* MinorColorNames[] = { "Blue", "Orange", "Green", "Brown", "Slate"};
