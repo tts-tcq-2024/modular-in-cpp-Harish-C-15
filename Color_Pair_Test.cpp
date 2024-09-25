cdc7512d1d0c787db71f2b8eefeda1620367cd33
Harish-C-15 <harish1ga18cs060@gmail.com>
2024-09-25 13:33:19 +0530

    Update Color_Pair_Test.cpp

--- Color_Pair_Test.cpp
+++ Color_Pair_Test.cpp
@@ -23,13 +23,4 @@ void testPairToNumber(
     assert(pairNumber == expectedPairNumber);
 }
 
-int main() {
-    testNumberToPair(4, WHITE, BROWN);
-    testNumberToPair(5, WHITE, SLATE);
-
-    testPairToNumber(BLACK, ORANGE, 12);
-    testPairToNumber(VIOLET, SLATE, 25);
-
-    return 0;
-}
 }
