b8e1df176bd4879a1b4aef089ac3550ec6035030
Harish-C-15 <harish1ga18cs060@gmail.com>
2024-09-25 13:34:01 +0530

    Update ColorPairManual.cpp

--- ColorPairManual.cpp
+++ ColorPairManual.cpp
@@ -29,6 +29,13 @@ void printColorCodingManual() {
 int main()
 {
     printColorCodingManual();
+    
+    testNumberToPair(4, WHITE, BROWN);
+    testNumberToPair(5, WHITE, SLATE);
+
+    testPairToNumber(BLACK, ORANGE, 12);
+    testPairToNumber(VIOLET, SLATE, 25);
+
     return 0;
 }
 }
