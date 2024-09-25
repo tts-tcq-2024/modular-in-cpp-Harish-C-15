ef9b19c79fd8de2386cd388a5a9f0dfdc3c1607d
Harish-C-15 <harish1ga18cs060@gmail.com>
2024-09-25 13:27:09 +0530

    Update ColorPairManual.cpp

--- ColorPairManual.cpp
+++ ColorPairManual.cpp
@@ -11,8 +11,8 @@ void printColorCodingManual() {
     std::cout << "-------------------------------------------\n";
 
     // Iterate through all combinations of major and minor colors
-    for (int majorIndex = 0; majorIndex < Color::numberOfMajorColors; ++majorIndex) {
-        for (int minorIndex = 0; minorIndex < Color::numberOfMinorColors; ++minorIndex) {
+    for (int majorIndex = 0; majorIndex < numberOfMajorColors; ++majorIndex) {
+        for (int minorIndex = 0; minorIndex < numberOfMinorColors; ++minorIndex) {
             // Create a ColorPair object
             ColorPair colorPair(static_cast<MajorColor>(majorIndex), static_cast<MinorColor>(minorIndex));
 
