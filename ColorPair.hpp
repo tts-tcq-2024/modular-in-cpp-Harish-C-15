f070dd01e68b6befad8efb9330831dbc23ffa55d
Harish-C-15 <harish1ga18cs060@gmail.com>
2024-09-25 13:25:21 +0530

    Update ColorPair.hpp

--- ColorPair.hpp
+++ ColorPair.hpp
@@ -3,6 +3,9 @@
 
 #include <iostream>
 
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
 extern const char* MajorColorNames[];
 extern const int numberOfMajorColors;
 extern const char* MinorColorNames[];
